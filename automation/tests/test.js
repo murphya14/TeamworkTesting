053ed67d9d31b66d96ef89adc87b2b77449a6304
murphya14 <aislingmurphy14@hotmail.com>
2022-02-24 20:11:15 +0000

    test

---
+++ automation/tests/test.js
@@ -0,0 +1,7 @@
+const { run, expect } = require('@playwright/test');
+
+run('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
